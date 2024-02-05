fa48df61680a83fd5d7e79bf34bf956e8a76310f
DeeptenduKuri <deeptendukuri@gmail.com>
2024-02-06 03:50:18 +0530

    Commit

--- src/App.js
+++ src/App.js
@@ -28,7 +28,8 @@ export default function Card() {
     flexWrap: "wrap",
     justifyContent: "center",
     alignItems: "center",
-    height: "100vh"
+    height: "100vh",
+    padding: "20px"
   };
 
   const imageStyle = {
@@ -52,7 +53,7 @@ export default function Card() {
           placeholder="Search countries"
           value={searchTerm}
           onChange={handleSearch}
-          style={{ width: "100%", paddingLeft: "40px" }}
+          style={{ width: "100%", paddingLeft: "40px", boxSizing: "border-box" }}
         />
         <div style={searchIconStyle} />
       </div>
@@ -71,13 +72,20 @@ export default function Card() {
 }
 
 const searchBoxStyle = {
-width:"80vw",
-margin:"10px",
+  width: "100%",
+  maxWidth: "80vw",
+  margin: "10px",
+  position: "relative"
 };
 
 const searchIconStyle = {
   position: "absolute",
   top: "50%",
   right: "10px",
-  transform: "translateY(-50%)"
+  transform: "translateY(-50%)",
+  width: "20px",
+  height: "20px",
+  backgroundImage: `url("data:image/svg+xml,<svg viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'><path d='M12.9 14.3l1.4 1.4 2.6-2.6c.4-.4.4-1 0-1.4l-2.6-2.6-1.4 1.4c-.2.2-.5.2-.7 0L8.3 9.3 5.9 6.9c-.2-.2-.5-.2-.7 0L4 8.7c-.4.4-.4 1 0 1.4L6.1 11.3l-2.6 2.6c-.2.2-.2 1 .1 1.4l1.4 1.4 3.1-3.1c.3-.3.3-1 0-1.4z' fill='%239CA3AF'/></svg>")`,
+  backgroundSize: "contain",
+  backgroundRepeat: "no-repeat"
 };
